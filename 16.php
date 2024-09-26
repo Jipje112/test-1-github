f542b2d276fa1763a290bc02f2344e62fe9dac3e
Jitse Swart <94473278+Jipje112@users.noreply.github.com>
2024-09-26 11:46:08 +0200

    Add files via upload

---
+++ 16.php
@@ -0,0 +1,32 @@
+<?php
+$age = 16;
+$number = 69;
+$plus= $age + $number;
+$multiply = $age * $number;
+$divide = $age / $number;
+$remove = $age - $number;
+?>
+<table>
+<tr>
+plus
+<?php echo $plus; ?>
+</tr>
+</table>
+<table>
+<tr>
+multiply
+<?php echo $multiply; ?>
+</tr>
+</table>
+<table>
+<tr>
+divide
+<?php echo $divide; ?>
+</tr>
+</table>
+<table>
+<tr>
+remove
+<?php echo $remove; ?>
+</tr>
+</table>
